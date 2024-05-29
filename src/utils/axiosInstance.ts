bb5881994f86812803c02cf24fb8246906aa42b0
Gherca Darius <ghercadarius@gmail.com>
2024-05-29 11:25:27 +0300

    tried fixing port again

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 const BASE_URL_HTTPS = "https://localhost:7225/";
-const BASE_URL_HTTP = "http://localhost:5012/";
+const BASE_URL_HTTP = "http://localhost:5013/";
 const axiosInstance = axios.create({
   baseURL: BASE_URL_HTTP, // Replace with your API base URL
   // You can add more default settings here, like headers
